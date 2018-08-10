02a5c675b4b2058ee6e0a3a37af36118c77140df
T r a m A n h <hipnguyen1997@gmail.com>
2018-08-10 10:13:21 +0700

    Create product 3

--- session/funtions.php
+++ session/funtions.php
@@ -1,7 +1,26 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: T r a m A n h
- * Date: 8/9/2018
- * Time: 3:54 PM
- */
+function get_current_url() {
+    $home_url = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+    return $home_url;
+}
+
+// Define constant
+define("HOME_URL", "http://localhost:8080/phptech/techmasterphp/techmasterphp/blog/");
+define("HOME_URL_ADMIN", "http://localhost:8080/phptech/techmasterphp/techmasterphp/blog/admin/");
+$home_dir_path = dirname(__FILE__);
+define("HOME_DIR_PATH", $home_dir_path);
+
+// Connect database
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "homework_8/9";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+?>
