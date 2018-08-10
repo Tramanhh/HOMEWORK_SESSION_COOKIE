50ad2625c990330da1c6f6f4ad230628471668a7
T r a m A n h <hipnguyen1997@gmail.com>
2018-08-10 10:13:46 +0700

    Create product 3

--- session/index.php
+++ session/index.php
@@ -1,6 +1,61 @@
 <?php
+include_once 'funtions.php';
+// Start the session
+session_start();
 
-$_SESSION["user"] = "batman";
-$_SESSION["pass"] = "abc123";
+    $sql = 'SELECT * FROM user';
+    if (isset($_SESSION['username']) && $_SESSION['username']) {
+        if ($stmt = $conn->query($sql)) {
+            while ($user = $stmt->fetch_assoc()) {
+                $user['username'] == $_SESSION['username'];
+                $user['password'] == $_SESSION['password'];
+            }
+        }
+        else {
+                echo '<pre>';
+                print_r($_SESSION);
+                echo '<pre>';
+            }
+
+} else {echo 'error';}
+    $conn->close();
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</head>
+<body>
+<div class="container">
+
+    <form name="login" action="http://localhost:8080/php/session/admin.php" class="form-signin" style="margin: 0 auto; width: 500px" method="post">
+        <h2 class="form-signin-heading">Please sign in</h2>
+        <label for="inputEmail" class="sr-only">Email address</label>
+        <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
+        <div class="checkbox">
+            <label>
+                <input type="checkbox" value="remember-me"> Remember me
+            </label>
+        </div>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+    </form>
+
+</div> <!-- /container -->
+
+</body>
+</html>
 
 
